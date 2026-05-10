a93d4453d4473182beb162207c229d80da58f8ef
esti2127 <elarrinagalopez@gmail.com>
2026-05-10 18:04:49 +0200

    diagrama de flujo: Galería Pexels

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//Crear 3 categorías de imágenes en la página de inicio
+
+//VARIABLES
+
+//Crear imágenes representantes de su categoría (con su contenedor)
+
+//Meter en otros contenedores las fotos relacionadas de la API
+
+//A cada uno de esos contenedores añadirle un botón que después permita añadir esa imagen a favoritos
+
+//EVENTOS
+
+//Evento click en documento con imágenes para que aparezcan las fotos de cada categoria > click en una foto --(API Pexels)-->  aparecen las fotos de esa categoria 
+
+//FUNCIONES
+
+//Función para que aparezcan las fotos de cada categoría al hacer click dividida en dos funciones:
+
+//Función 1: La que se encarga de recoger el fetch 
+
+//Función 2: La que pinte las fotos en el DOM ----> recibe el array de las fotos y las pinta
+
+//Función para guardar los favoritos en el localStorage
+
+//Función para poder eliminar las imágenes de la sección de favoritos 
+
+
+
+//Saver para que las fotos persistan incluso aunque se cierre la página
+
