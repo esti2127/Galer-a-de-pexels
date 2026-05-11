7bd98cf1b836112538fc15ee7d7c878247095804
esti2127 <elarrinagalopez@gmail.com>
2026-05-11 16:01:21 +0200

    archivos con funcion de categorias de imagenes

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ const primeraCategoria = document.querySelector('#boton1')
 const segundaCategoria = document.querySelector('#boton2')
 const terceraCategoria = document.querySelector('#boton3')
 
+const numberAPI = "fGhFoAuT6joFd5Xg1VpciXCPoGTi8Jgbanp1do5pSfXSlu1rzwBjPAi6";
+
+const arrayTodascategorias = [primeraCategoria, segundaCategoria, terceraCategoria];
+
 
 
 const boton = document.createElement("BUTTON")
@@ -17,7 +21,7 @@ const imagen = document.createElement("img")
 
 const contenedorButton = document.getElementById("contenedorBotones")
 
-const arrayCategorias = ["naturaleza", "animales", "plantas"];
+const arrayCategorias = ["nature", "animals", "plants"];
 //const arrayCategorias = [{id: "nature", name: "naturaleza"}, {id: "animals", name: "animales"}, {id: "plants", name: "plantas"}];
 //const API_KEY = 'fGhFoAuT6joFd5Xg1VpciXCPoGTi8Jgbanp1do5pSfXSlu1rzwBjPAi6'
 
@@ -36,11 +40,18 @@ contenedorButton.append(boton)
 
 
 
+//Crear 3 categorías de imágenes en la página de inicio
+
+
+//VARIABLES
+
+//Crear imágenes representantes de su categoría (con su contenedor)
 
 
 
+//Meter en otros contenedores las fotos relacionadas de la API
 
-
+//A cada uno de esos contenedores añadirle un botón que después permita añadir esa imagen a favoritos
 
 
 
@@ -87,31 +98,20 @@ const agregarFavorito = favoritoElegido => {
 
 }
 
-//Crear 3 categorías de imágenes en la página de inicio
-
-
-//VARIABLES
-
-//Crear imágenes representantes de su categoría (con su contenedor)
-
-
-
-//Meter en otros contenedores las fotos relacionadas de la API
-
-//A cada uno de esos contenedores añadirle un botón que después permita añadir esa imagen a favoritos
-
 //EVENTOS
 
 //Evento click en documento con imágenes para que aparezcan las fotos de cada categoria > click en una foto --(API Pexels)-->  aparecen las fotos de esa categoria 
 
-document.addEventListener('click', () => {
+document.addEventListener('click', (ev) => {
   const inputSearch = document.getElementById('image-search');
 
 
 
 
 
+})
 
+contenedorBotones.addEventListener('submit', (ev) => {
 
 
 
@@ -136,7 +136,8 @@ document.addEventListener('click', () => {
 const getallImages = async (endpoint) => {
   try {
 
-    const resp = fetch(`https://api.pexels.com/v1/${endpoint}`, { headers: { "Authorization": "fGhFoAuT6joFd5Xg1VpciXCPoGTi8Jgbanp1do5pSfXSlu1rzwBjPAi6" } });
+    const resp = await fetch(`https://api.pexels.com/v1/${endpoint}&per_page=1`, { headers: { "Authorization": numberAPI } });
+    //console.log(resp)
     if (!resp.ok) {
 
       throw (resp.status);
@@ -158,24 +159,49 @@ const getallImages = async (endpoint) => {
 
 /**Obtener fotos por id*/
 
+//Función para que aparezcan las fotos de cada categoría al hacer click dividida en dos funciones:
 
-const getallImagesporcategoria = async (id) => {
+//Función 1: La que se encarga de recoger el fetch 
+
+
+const getallImagesporcategoria = async (categoria, index) => {
+
+  arrayCategorias.forEach(async categoria => { 
+
   try {
-    const resp2 = fetch(`https://api.pexels.com/v1/photos/${id}`)
-  if (!resp.ok) {
+    const resp2 = await fetch(`https://api.pexels.com/v1/search?query=${categoria}&per_page=1`, { headers: { "Authorization": numberAPI } })
+    
+  if (!resp2.ok) {
 
-    throw (resp.status);
+    throw (resp2.status);
   }
-  const data = await resp.json();
-  return data
+  const data = await resp2.json();
+  console.log(data.photos)
+
+  pintarFotos(arrayTodascategorias[index]);
+
   }
   catch (error) {
   console.log(`Error ${error}`)
 }
+
+})
   
 }
 
-arrayCategorias.forEach(categoria => { getallImages(categoria) });
+
+
+
+
+
+
+//Función 2: La que pinte las fotos en el DOM ----> recibe el array de las fotos y las pinta
+
+
+const pintarFotos = () => { }
+
+
+
 
 /**Obtener fotos por orientación */
 
@@ -204,13 +230,11 @@ boton.append(getallImages)
 console.log(getallImages)
 
 
-//Función para que aparezcan las fotos de cada categoría al hacer click dividida en dos funciones:
 
-//Función 1: La que se encarga de recoger el fetch 
 
-//Función 2: La que pinte las fotos en el DOM ----> recibe el array de las fotos y las pinta
 
-const pintarFotos = () => { }
+
+
 
 //Función para guardar los favoritos en el localStorage
 
@@ -235,3 +259,12 @@ const eliminarFavorito = () => {
 
 
 
+
+
+
+
+
+
+
+
+getallImagesporcategoria();
