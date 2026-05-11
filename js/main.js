47708ee269802a36586a6333c9ed9e3d6aafb6fb
esti2127 <elarrinagalopez@gmail.com>
2026-05-11 10:12:35 +0200

    primera función click imagenes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
+const imageSearch = document.querySelector('#image-search');
+const imageOrientacion = document.querySelector('#orientacion');
+const imageOrientacion = document.querySelector('.boton');
+
+const arrayBotones = [
+  {
+    id:0,
+    tag:naturaleza,
+  }
+]
+
+
+
+const boton = createElement("BUTTON")
+
+boton.classList.add("boton")
+
+const imagen = createElement("img")
+
+const contenedorButton = document.getElementById("contenedorBotones")
+
+
+
+
 //Crear 3 categorías de imágenes en la página de inicio
 
 //VARIABLES
 
 //Crear imágenes representantes de su categoría (con su contenedor)
+
+
 
 //Meter en otros contenedores las fotos relacionadas de la API
 
@@ -12,7 +38,33 @@
 
 //Evento click en documento con imágenes para que aparezcan las fotos de cada categoria > click en una foto --(API Pexels)-->  aparecen las fotos de esa categoria 
 
+document.addEventListener('click', () => {
+  const inputSearch = document.getElementById('image-search');
+})
+
+
+
 //FUNCIONES
+
+const getallImages = async (tag) => {
+  try {
+    const resp = fetch('https://api.pexels.com/v1/search');
+    if (!resp.ok) {
+        
+            throw (resp.status);
+        }
+    const data = await resp.json();
+    return data
+  }
+  catch(error){
+    console.log(`Error ${error}`)
+  }
+}
+
+getallImages().then(() => {
+    console.log(); 
+
+});
 
 //Función para que aparezcan las fotos de cada categoría al hacer click dividida en dos funciones:
 
